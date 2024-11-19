75117d062d9bb11a445333fdbd08622c39c6177d
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-11-20 00:11:26 +0700

    install and setup tailwindcss with shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "esnext",
+    outDir: ".react",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@libs": path.resolve(__dirname, "./src/libs"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@customTypes": path.resolve(__dirname, "./src/types"),
+      "@public": path.resolve(__dirname, "./src/public"),
+    },
+  },
 });
