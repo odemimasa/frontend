b8020140025037acab94f0efa992bd2aad83abcb
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-11-22 12:56:56 +0700

    create Hero and VisiMisi component for Home page

---
+++ src/components/Home/VisiMisi.tsx
@@ -0,0 +1,27 @@
+function VisiMisi() {
+  return (
+    <div className="my-32 mx-8">
+      <article className="text-center">
+        <h2 className="text-[#61412D] font-bold text-4xl mb-6">Visi</h2>
+        <p className="text-[#363636]">
+          Menjadikan digitalisasi dalam kepekaan beribadah di setiap kesibukan
+          apapun
+        </p>
+      </article>
+
+      <hr className="border-[#C2C2C2] my-12" />
+
+      <article className="text-center">
+        <h2 className="text-[#61412D] font-bold text-4xl mb-6">Misi</h2>
+        <p className="text-[#363636]">
+          Mewujudkan kepekaan dalam beribadah dengan digitalisasi demi
+          tercapainya kehidupan yang lebih bermakna. Membawa perubahan bahwa
+          ibadah perlu disimak dan direfleksikan secara nyata melalui
+          digitalisasi.
+        </p>
+      </article>
+    </div>
+  );
+}
+
+export { VisiMisi };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { VisiMisi } from "@components/Home/VisiMisi";
 import { Button } from "@components/shadcn/Button";
 import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
 import { useToast } from "@hooks/shadcn/useToast";
@@ -27,6 +28,16 @@ export default function Home(): JSX.Element {
 
   return (
     <>
+      <div className="bg-[url('/hero.jpg')] h-screen bg-cover bg-center">
+        <div className="relative z-10 h-full flex flex-col justify-center items-center mx-8">
+          <p className="text-white font-bold text-center text-4xl ">
+            Mengingatkan Ibadah, Mengingatkan Kebaikan
+          </p>
+        </div>
+      </div>
+
+      <VisiMisi />
+
       <Button onClick={handleLogin} variant="outline">
         Login With Google
       </Button>
