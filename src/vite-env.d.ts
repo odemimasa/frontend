d09a5fbab0f1b4e14b13f2720f7383393edeb319
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-12-23 20:22:38 +0700

    add env variables example upon rebased firebase commit interactively

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,13 @@
 
 interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string;
+  readonly FIREBASE_API_KEY: string;
+  readonly FIREBASE_AUTH_DOMAIN: string;
+  readonly FIREBASE_PROJECT_ID: string;
+  readonly FIREBASE_STORAGE_BUCKET: string;
+  readonly FIREBASE_MESSAGE_SENDER_ID: string;
+  readonly FIREBASE_APP_ID: string;
+  readonly FIREBASE_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
