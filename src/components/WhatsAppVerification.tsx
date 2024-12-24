96dba30c5f5e65bf7e7ba353969c7ca8df19371d
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-12-24 17:16:48 +0700

    update UX when filling WhatsApp number

--- src/components/WhatsAppVerification.tsx
+++ src/components/WhatsAppVerification.tsx
@@ -47,7 +47,7 @@ function WhatsAppVerification({ setOpen }: WhatsAppVerificationProps) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      phoneNumber: user!.phoneNumber,
+      phoneNumber: user!.phoneNumber !== "" ? user!.phoneNumber : "+62",
       pin: "",
     },
   });
