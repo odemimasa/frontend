a4f9e9676ecd4c5c367295029c4dda396db47321
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-05-11 12:23:56 +0700

    enable PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,51 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Demi Masa",
+        short_name: "Demi Masa",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#FFFFFF",
+        theme_color: "#FFFFFF",
+        description:
+          "Menjadikan digitalisasi dalam kepekaan beribadah di setiap kesibukan apapun.",
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
